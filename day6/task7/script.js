f214a804745df3388385b1cd8adcc721a11d0093
milanlophyWork <lophymilan@gmail.com>
2025-10-21 21:08:58 +0530

    day 6 tasks

--- day6/task7/script.js
+++ day6/task7/script.js
@@ -1,13 +1,19 @@
 // Flatten nested arrays
-let newArr=[]
-function flatten(arr){
-    for(item of arr){
-        if(Array.isArray(item)){
-            flatten(item)
-        }else{
-            newArr.push(item)
+function wrap(arr){
+    let newArr=[]
+    
+    function flatten(arr){
+        for(item of arr){
+            if(Array.isArray(item)){
+                flatten(item)
+            }else{
+                newArr.push(item)
+            }
         }
+        return newArr
     }
-    return newArr
+    return flatten(arr)
 }
-console.log(flatten([1,[2,[3,[4]],5]]))
+
+console.log(wrap([1,[2,[3,[4]],5]]))
+console.log(wrap([2,[4,5]]))
